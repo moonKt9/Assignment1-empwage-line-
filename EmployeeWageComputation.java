8703125e422799debf81ddfd31d8f734b591df6f
Kritika <kritistarsmind@gmail.com>
2022-07-02 08:08:33 +0530

    Employee Wage comutation

---
+++ EmployeeWageComputation.java
@@ -0,0 +1,8 @@
+package com.practiceproblem;
+
+public class EmployeeWageComputation {
+	
+public static void main(String args[]) {
+	System.out.println("Welcome to Employee Wage Computation");
+}
+}
