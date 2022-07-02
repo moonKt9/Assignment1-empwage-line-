c13b3f59d68284d59a834489a986c00047d05838
Kritika <kritistarsmind@gmail.com>
2022-07-02 21:24:36 +0530

    switchcase use

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,27 +1,28 @@
-package com.practiceproblem;//add part time employee wage
+package com.practiceproblem;//using switch case
 
 public class EmployeeWageComputation {
 	
-public static void main(String args[]) {
-	System.out.println("Welcome to Employee Wage Computation");
-	int EMP_RATE_PER_HOUR=20;
-	int IS_PART_TIME=1;
+
+	public static final int IS_FULL_TIME=2;
+	public static final int EMP_RATE_PER_HOUR=20;
+	public static final int IS_PART_TIME=1;
+	public static void main(String args[]) {
 	int empHrs=0;
 	int empWage=0;
-	int IS_FULL_TIME=2;
-	double empCheck;
-	empCheck=Math.floor(Math.random()*10)%2;
-	
-	if(empCheck==IS_PART_TIME)
+	int empCheck=(int)(Math.floor(Math.random())*10)%3;
+	switch(empCheck) {
+	case IS_PART_TIME:
 		empHrs=4;
-		
-	else if(empCheck==IS_FULL_TIME)
+		break;
+	case IS_FULL_TIME:
 		empHrs=8;
-	else
-		empHrs=0;
+		break;
+		
+	}
 	
 	empWage=empHrs*EMP_RATE_PER_HOUR;
 	System.out.println("empWage:"+empWage);
+	
 	
 }
 }
