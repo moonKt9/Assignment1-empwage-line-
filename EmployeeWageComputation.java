0eb2e47fba819bd2f980a794a2adfda3513ab1dc
Kritika <kritistarsmind@gmail.com>
2022-07-02 20:52:21 +0530

    part time emp wage

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,21 +1,25 @@
-package com.practiceproblem;//calculate daily employee wage
+package com.practiceproblem;//add part time employee wage
 
 public class EmployeeWageComputation {
 	
 public static void main(String args[]) {
 	System.out.println("Welcome to Employee Wage Computation");
 	int EMP_RATE_PER_HOUR=20;
-	int empHrs;
-	int empWage;
-	int IS_FULL_TIME=1;
+	int IS_PART_TIME=1;
+	int empHrs=0;
+	int empWage=0;
+	int IS_FULL_TIME=2;
 	double empCheck;
 	empCheck=Math.floor(Math.random()*10)%2;
 	
-	if(empCheck==IS_FULL_TIME)
-		empHrs=8;
+	if(empCheck==IS_PART_TIME)
+		empHrs=4;
 		
+	else if(empCheck==IS_FULL_TIME)
+		empHrs=8;
 	else
 		empHrs=0;
+	
 	empWage=empHrs*EMP_RATE_PER_HOUR;
 	System.out.println("empWage:"+empWage);
 	
