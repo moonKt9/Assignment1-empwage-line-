517659a7972e8db11ad184cc766b7841ddd19f17
Kritika <kritistarsmind@gmail.com>
2022-07-02 21:51:38 +0530

    wages till number of working days

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -7,9 +7,20 @@ public class EmployeeWageComputation {
 	public static final int EMP_RATE_PER_HOUR=20;
 	public static final int IS_PART_TIME=1;
 	public static final int NUM_WORKING_DAYS=2;
+	public static final int MAX_HRS_IN_MONTH=10;
+	
+	
 	public static void main(String args[]) {
 	int empHrs=0;
 	int empWage=0;
+	int totalEmpHrs=0;
+	int totalWorkingDays=0;
+	while ( totalEmpHrs<=MAX_HRS_IN_MONTH &&
+			totalWorkingDays< NUM_WORKING_DAYS){
+		totalWorkingDays++;
+		
+	}
+			
 	for (int day=0;day<NUM_WORKING_DAYS;day++) {
 		
 	int empCheck=(int)(Math.floor(Math.random())*10)%3;
